222b83422625556ef688f569975b0c9683598d63
ktbaek <kristofferbaek@gmail.com>
2025-09-28 13:59:31 +0200

    Stop tracking tests/testhat/, added to .gitignore

--- tests/testthat/test_normalize_year.R
+++
@@ -1,5 +0,0 @@
-test_that("normalize_year turns years out of range into NA, leaves others", {
-  df  <- tibble::tibble(uuid=c("a","b","c"), planteaar=c(0L, 1999L, NA))
-  out <- normalize_year(df)
-  expect_equal(out$planteaar, c(NA_integer_, 1999L, NA_integer_))
-})
